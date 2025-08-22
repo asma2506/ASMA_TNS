8bdca279d65e8c434c5c38e0dff4bbea6969d041
welcome <welcome@AsmaAkbarali>
2025-08-22 19:09:42 +0530

    commit

---
+++ src/model/Person.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Person {
+	
+	    protected String name;
+	    protected String address;
+	    protected String contact;
+
+	    public Person(String name, String address, String contact) {
+	        this.name = name;
+	        this.address = address;
+	        this.contact = contact;
+	    }
+
+	    public String getName() {
+	    	return name; 
+	    	}
+	    public void setName(String name) { 
+	    	this.name = name; 
+	    	}
+
+	    public String getAddress() {
+	    	return address; }
+	    public void setAddress(String address) {
+	    	this.address = address;
+	    	}
+
+	    public String getContact() {
+	    	return contact; 
+	    	}
+	    public void setContact(String contact) { 
+	    	this.contact = contact; 
+	    	}
+
+	    @Override
+	    public String toString() {
+	        return "Name: " + name + ", Address: " + address + ", Contact: " + contact;
+	    }
+	}
+
+
+
---
+++ src/service/BankingService.java
@@ -0,0 +1,27 @@
+package service;
+
+
+
+import model.*;
+import java.util.*;
+
+public interface BankingService {
+    void addCustomer(Customer customer);
+    void addAccount(Account account);
+    void addTransaction(Transaction transaction);
+    void addBeneficiary(Beneficiary beneficiary);
+
+    Customer findCustomerById(int id);
+    Account findAccountById(int id);
+    Transaction findTransactionById(int id);
+    Beneficiary findBeneficiaryById(int id);
+
+    List<Account> getAccountsByCustomerId(int customerId);
+    List<Transaction> getTransactionsByAccountId(int accountId);
+    List<Beneficiary> getBeneficiariesByCustomerId(int customerId);
+
+    Collection<Customer> getAllCustomers();
+    Collection<Account> getAllAccounts();
+    Collection<Transaction> getAllTransactions();
+    Collection<Beneficiary> getAllBeneficiaries();
+}
